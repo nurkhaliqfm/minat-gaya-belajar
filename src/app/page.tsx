127b21b9bdb124b6236e6db1d1330183aef03d4d
nurkhaliqfm <nurkhaliqfuthra@gmail.com>
2024-05-29 21:05:57 +0800

    feat: user contact and biodata dialog form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import CircleProgress from "@/components/customs/circle-progress";
-import Loading from "@/components/customs/loading";
-import { useToast } from "@/components/ui/use-toast";
+import BiodataDialog from "@/components/customs/biodata-dialog";
 import { cn } from "@/lib/utils";
+import { cookies } from "next/headers";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import { HashLoader } from "react-spinners";
 
 interface EventsProps {
   id?: number;
@@ -17,9 +17,16 @@ interface EventsProps {
   };
 }
 
+export interface DataBiodata {
+  hp?: string;
+  isKuliah?: number;
+}
+
 export default function Home() {
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
+  const [dataBiodata, setDataBiodata] = useState<DataBiodata>();
+  const [biodataAlertState, setBiodataAlertState] = useState(false);
   const [events, setEvents] = useState<Array<EventsProps>>([]);
 
   const listAssets: any = {
@@ -33,55 +40,74 @@ export default function Home() {
     },
   };
 
-  const handleOptionOnClick = (value: number) => {};
-
   useEffect(() => {
     setIsLoading(true);
-    fetch("/api/event", { method: "GET" })
+    fetch("/api/me", { method: "GET" })
       .then((res) => res.json())
       .then((res) => {
-        setEvents(res.data);
-      })
-      .finally(() => {
-        setIsLoading(false);
+        if (res.data.hp === null || res.data.isKuliah === null) {
+          setBiodataAlertState(true);
+          setDataBiodata(res.data);
+        }
+
+        fetch("/api/event", { method: "GET" })
+          .then((res) => res.json())
+          .then((res) => {
+            setEvents(res.data);
+          })
+          .finally(() => {
+            setIsLoading(false);
+          });
       });
   }, []);
 
   return (
     <>
+      {dataBiodata && (
+        <BiodataDialog state={biodataAlertState} data={dataBiodata} />
+      )}
+
       <main className="flex h-full justify-center">
         <section className="w-full bg-white h-full rounded-2xl shadow-lg max-w-[1200px] p-4 md:p-10">
-          <div className="flex flex-col md:flex-row gap-4">
-            {events.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className={cn(
-                    "cursor-pointer shadow-md my-2 px-4 py-2 rounded-xl font-medium flex flex-col items-center hover:border-2 hover:border-primary"
-                  )}
-                  onClick={() => {
-                    localStorage.removeItem(
-                      `${listAssets[item.name!].slug}-${item.id}-status`
-                    );
-                    router.push(
-                      "/" + listAssets[item.name!].slug + "/" + item.id
-                    );
-                  }}
-                >
-                  <div className="rounded-xl overflow-hidden mt-4">
-                    <Image
-                      src={"/img/" + listAssets[item.name!].img}
-                      alt={item.name ?? ""}
-                      width={120}
-                      height={120}
-                    />
-                  </div>
+          {isLoading ? (
+            <div className="flex justify-center items-center flex-col  h-full p-2">
+              <div className=" p-6 rounded-lg flex flex-col gap-6 justify-center items-center">
+                <HashLoader color="#4245D1" />
+              </div>
+            </div>
+          ) : (
+            <div className="flex flex-col md:flex-row gap-4">
+              {events.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={cn(
+                      "cursor-pointer shadow-md my-2 px-4 py-2 rounded-xl font-medium flex flex-col items-center hover:border-2 hover:border-primary"
+                    )}
+                    onClick={() => {
+                      localStorage.removeItem(
+                        `${listAssets[item.name!].slug}-${item.id}-status`
+                      );
+                      router.push(
+                        "/" + listAssets[item.name!].slug + "/" + item.id
+                      );
+                    }}
+                  >
+                    <div className="rounded-xl overflow-hidden mt-4">
+                      <Image
+                        src={"/img/" + listAssets[item.name!].img}
+                        alt={item.name ?? ""}
+                        width={120}
+                        height={120}
+                      />
+                    </div>
 
-                  <span className="my-2">{item.name}</span>
-                </div>
-              );
-            })}
-          </div>
+                    <span className="my-2">{item.name}</span>
+                  </div>
+                );
+              })}
+            </div>
+          )}
           <div></div>
         </section>
       </main>
