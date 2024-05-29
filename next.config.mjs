de181c8c81e9425c1b1a19055e57820106b4ca41
nurkhaliqfm <nurkhaliqfuthra@gmail.com>
2024-05-30 01:34:50 +0800

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: false };
+const nextConfig = { trailingSlash: true, reactStrictMode: false };
 
 export default nextConfig;
